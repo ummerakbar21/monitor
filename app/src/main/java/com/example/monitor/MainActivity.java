a61bccc02a905c31eb1ebd73f6452214f7ad001f
Wahdat Jan <Wahdat.jan@trialx.com>
2019-10-30 23:18:52 +0530

    Create login UI

--- app/src/main/java/com/example/monitor/MainActivity.java
+++ app/src/main/java/com/example/monitor/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.monitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
