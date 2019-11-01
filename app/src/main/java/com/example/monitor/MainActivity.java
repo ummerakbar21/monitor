5e32be95490a3dbc59ac1251f9db5374615d1453
Wahdat Jan <Wahdat.jan@trialx.com>
2019-11-01 23:11:55 +0530

    create UI for login

--- app/src/main/java/com/example/monitor/MainActivity.java
+++ app/src/main/java/com/example/monitor/MainActivity.java
@@ -3,14 +3,21 @@ package com.example.monitor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.text.style.TextAppearanceSpan;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
+ TextView marqueTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        marqueTv = findViewById(R.id.marque_text);
+        marqueTv.setSelected(true);
+        marqueTv.setEllipsize(TextUtils.TruncateAt.MARQUEE);
+        marqueTv.setSingleLine(true);
     }
 }
