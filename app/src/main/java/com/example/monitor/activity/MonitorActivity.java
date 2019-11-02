c28911d9313a5367bb22794082fc07fbc7481357
Wahdat Jan <Wahdat.jan@trialx.com>
2019-11-02 18:45:05 +0530

    UI changed

--- app/src/main/java/com/example/monitor/activity/MonitorActivity.java
+++ app/src/main/java/com/example/monitor/activity/MonitorActivity.java
@@ -3,14 +3,20 @@ package com.example.monitor.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 import com.example.monitor.R;
 
 public class MonitorActivity extends AppCompatActivity {
 
+    Switch simpleSwitch;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
+        simpleSwitch = findViewById(R.id.status_Switch); // initiate Switch
+
+        simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
+        simpleSwitch.setTextOff("Off");
     }
 }
