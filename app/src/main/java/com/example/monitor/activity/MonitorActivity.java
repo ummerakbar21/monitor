947afdce93e68a13d2f47c4f4d3ac07a3e8cad54
Wahdat Jan <Wahdat.jan@trialx.com>
2019-11-02 20:17:02 +0530

    full ui created

--- app/src/main/java/com/example/monitor/activity/MonitorActivity.java
+++ app/src/main/java/com/example/monitor/activity/MonitorActivity.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.monitor.R;
 
 public class MonitorActivity extends AppCompatActivity {
 
     Switch simpleSwitch;
+    TextView progressTv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitor);
         simpleSwitch = findViewById(R.id.status_Switch); // initiate Switch
+        progressTv = findViewById(R.id.switch_label_tv);
 
-        simpleSwitch.setTextOn("On"); // displayed text of the Switch whenever it is in checked or on state
-        simpleSwitch.setTextOff("Off");
+
+
     }
 }
