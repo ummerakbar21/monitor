277aa6d8bb02ea8aa91c044d20c5e189cc562401
ummerakbar45 <akbar@trialx.com>
2019-11-03 12:31:38 +0530

    added foundation all 6

--- app/src/main/java/com/example/monitor/activity/FoundationActivity.java
+++ app/src/main/java/com/example/monitor/activity/FoundationActivity.java
@@ -16,7 +16,7 @@ import com.github.mikephil.charting.utils.ColorTemplate;
 import java.util.ArrayList;
 
 public class FoundationActivity extends AppCompatActivity {
-    PieChart pieChart;
+    PieChart pieChart, pieChart2, pieChart3, pieChart4, pieChart5, pieChart6;
     PieData pieData;
     PieDataSet pieDataSet;
     ArrayList pieEntries;
@@ -26,23 +26,55 @@ public class FoundationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_foundation);
 
-        pieChart = findViewById(R.id.piechart);
         getEntries();
-        pieDataSet = new PieDataSet(pieEntries, "");
-        pieData = new PieData(pieDataSet);
-        pieChart.setData(pieData);
-        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
-        pieDataSet.setSliceSpace(2f);
-        pieDataSet.setValueTextColor(Color.BLACK);
-        pieDataSet.setValueTextSize(6f);
         Description description =new Description();
         description.setText("");
+
+
+
+
+        pieChart = findViewById(R.id.piechart);
+        pieChart.setData(pieData);
         pieChart.setDescription(description);
+
+        pieChart2 = findViewById(R.id.piechart2);
+        pieChart2.setData(pieData);
+        pieChart2.setDescription(description);
+
+        pieChart3 = findViewById(R.id.piechart3);
+        pieChart3.setData(pieData);
+        pieChart3.setDescription(description);
+
+        pieChart4 = findViewById(R.id.piechart4);
+        pieChart4.setData(pieData);
+        pieChart4.setDescription(description);
+
+        pieChart5 = findViewById(R.id.piechart5);
+        pieChart5.setData(pieData);
+        pieChart5.setDescription(description);
+
+        pieChart6 = findViewById(R.id.piechart6);
+        pieChart6.setData(pieData);
+        pieChart6.setDescription(description);
+
+
+
+
+
         pieDataSet.setSliceSpace(5f);
     }
     private void getEntries() {
         pieEntries = new ArrayList<>();
         pieEntries.add(new PieEntry(2f,"used", 0));
         pieEntries.add(new PieEntry(4f, "available",1));
+
+
+        pieDataSet = new PieDataSet(pieEntries, "");
+        pieData = new PieData(pieDataSet);
+        pieDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        pieDataSet.setSliceSpace(2f);
+        pieDataSet.setValueTextColor(Color.BLACK);
+        pieDataSet.setValueTextSize(6f);
+
     }
 }
